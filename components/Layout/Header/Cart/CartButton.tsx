b48d27d82bf49c7139be54a36b015a5ee0a0b590
Joseph Collicoat <jcollicoat@gmail.com>
2024-10-19 17:09:48 +0100

    checkout button

--- components/Layout/Header/Cart/CartButton.tsx
+++ components/Layout/Header/Cart/CartButton.tsx
@@ -1,14 +1,20 @@
 'use client';
 
 import { FC } from 'react';
+import { PiShoppingBag } from 'react-icons/pi';
 import styles from './CartButton.module.scss';
 
 export const CartButton: FC = () => {
-    const text = 'Cart Empty';
+    const text = 'Checkout';
+    const sub = '2 items';
 
     return (
         <button className={styles.button} onClick={() => alert('Clicked!')}>
-            {text}
+            <PiShoppingBag size={24} />
+            <div className={styles.text}>
+                <span>{text}</span>
+                <span className={styles.subtext}>{sub}</span>
+            </div>
         </button>
     );
 };

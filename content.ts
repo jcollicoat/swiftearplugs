a42fb455d97ad49f1d8b4499d65b323a06cf5292
Joseph Collicoat <jcollicoat@gmail.com>
2024-10-20 09:22:22 +0100

    hero quote content

--- content.ts
+++ content.ts
@@ -5,5 +5,9 @@ export const content = {
         title: 'Live, laugh<br />and live loudly.',
         subtitle: 'Swift&nbsp;Earplugs have you covered.',
         intro: 'Dive into the music with our high-fidelity earplugs designed to deliver crystal clear sound, while protecting your hearing for many years to come.',
+        testimonial: {
+            quote: 'I use these at really loud concerts, and they not only protect my ears but actually make the music sound better by balancing the sound and reducing the noise from the crowd.',
+            name: 'Amber M.',
+        },
     },
 };

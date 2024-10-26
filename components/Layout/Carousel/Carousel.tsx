e47e35691f8ade195f6c703358610dd68eb659ed
Joseph Collicoat <jcollicoat@gmail.com>
2024-10-26 18:49:22 +0100

    desktop carousel

--- components/Layout/Carousel/Carousel.tsx
+++ components/Layout/Carousel/Carousel.tsx
@@ -2,7 +2,7 @@
 
 import classNames from 'classnames';
 import Image from 'next/image';
-import { FC, useEffect, useState } from 'react';
+import { FC, Fragment, useEffect, useState } from 'react';
 import { content } from 'content';
 import styles from './Carousel.module.scss';
 
@@ -31,73 +31,95 @@ export const Carousel: FC = () => {
                 slide % 2 !== 0 && styles.alternate,
             )}
         >
-            <div className={styles.header}>
-                <h2
-                    // eslint-disable-next-line react/no-danger
-                    dangerouslySetInnerHTML={{
-                        __html: content.carousel.heading,
-                    }}
-                />
-                <Image
-                    src="/blacks-1.png"
-                    alt=""
-                    width={181 / 2}
-                    height={183 / 2}
-                    className={styles.float1}
-                />
-            </div>
-            <div className={styles.carousel}>
-                <div className={styles.imageContainer}>
-                    {content.carousel.items.map((item, index) => (
-                        <Image
-                            key={item.heading}
-                            src={`${item.image}-m.png`}
-                            alt=""
-                            width={1404 / 2}
-                            height={876 / 2}
-                            className={classNames(
-                                styles.image,
-                                index === slide && styles.active,
-                            )}
-                        />
-                    ))}
-                    <div className={styles.dots}>
+            <div className={styles.content}>
+                <div className={styles.header}>
+                    <h2
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{
+                            __html: content.carousel.heading,
+                        }}
+                    />
+                    <Image
+                        src="/blacks-1.png"
+                        alt=""
+                        width={181 / 2}
+                        height={183 / 2}
+                        className={styles.float1}
+                    />
+                    <Image
+                        src="/pinks-3.png"
+                        alt=""
+                        width={240 / 2}
+                        height={163 / 2}
+                        className={styles.float2}
+                    />
+                </div>
+                <div className={styles.carousel}>
+                    <div className={styles.imageContainer}>
                         {content.carousel.items.map((item, index) => (
-                            <button
+                            <Fragment key={item.heading}>
+                                <Image
+                                    src={`${item.image}-m.jpg`}
+                                    alt=""
+                                    width={1404 / 2}
+                                    height={876 / 2}
+                                    className={classNames(
+                                        styles.image,
+                                        styles.small,
+                                        index === slide && styles.active,
+                                    )}
+                                />
+                                <Image
+                                    src={`${item.image}-d.jpg`}
+                                    alt=""
+                                    width={1404 / 2}
+                                    height={876 / 2}
+                                    className={classNames(
+                                        styles.image,
+                                        styles.large,
+                                        index === slide && styles.active,
+                                    )}
+                                />
+                            </Fragment>
+                        ))}
+                        <div className={styles.dots}>
+                            {content.carousel.items.map((item, index) => (
+                                <button
+                                    key={item.heading}
+                                    onClick={() => {
+                                        setAnimate(false);
+                                        setSlide(index);
+                                    }}
+                                    className={classNames(
+                                        styles.dot,
+                                        index === slide && styles.active,
+                                    )}
+                                    title={item.heading}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                    <div
+                        className={classNames(
+                            styles.items,
+                            !animate && styles.static,
+                        )}
+                    >
+                        {content.carousel.items.map((item, index) => (
+                            <div
                                 key={item.heading}
-                                onClick={() => {
-                                    setAnimate(false);
-                                    setSlide(index);
-                                }}
                                 className={classNames(
-                                    styles.dot,
+                                    styles.item,
                                     index === slide && styles.active,
+                                    index === previousSlide && styles.previous,
+                                    index === nextSlide && styles.next,
                                 )}
-                                title={item.heading}
-                            />
+                            >
+                                <h3>{item.heading}</h3>
+                                <p>{item.content}</p>
+                            </div>
                         ))}
                     </div>
-                </div>
-                <div
-                    className={classNames(
-                        styles.content,
-                        !animate && styles.static,
-                    )}
-                >
-                    {content.carousel.items.map((item, index) => (
-                        <div
-                            key={item.heading}
-                            className={classNames(
-                                styles.item,
-                                index === slide && styles.active,
-                                index === previousSlide && styles.previous,
-                                index === nextSlide && styles.next,
-                            )}
-                        >
-                            <h3>{item.heading}</h3>
-                            <p>{item.content}</p>
-                        </div>
-                    ))}
                 </div>
             </div>
         </section>

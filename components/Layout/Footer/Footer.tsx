b4f699cfd9ee5da11c828445569df2bd298cf05d
Joseph Collicoat <jcollicoat@gmail.com>
2024-10-27 12:13:12 +0000

    add product link to footer

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -8,18 +8,25 @@ export const Footer: FC = () => (
     <footer className={styles.footer}>
         <div className={styles.content}>
             <nav>
-                <Link href="/" className={styles.logo}>
-                    <Logo />
-                </Link>
-                <Link href="/privacy-policy" className={styles.link}>
-                    Privacy Policy
-                </Link>
-                <Link href="/terms-of-service" className={styles.link}>
-                    Terms of Service
-                </Link>
-                <Link href="/returns-plolicy" className={styles.link}>
-                    Returns Policy
-                </Link>
+                <div className={styles.primary}>
+                    <Link href="/" className={styles.logo}>
+                        <Logo />
+                    </Link>
+                    <Link href="/#product" className={styles.link}>
+                        Choose Your Colour
+                    </Link>
+                </div>
+                <div className={styles.secondary}>
+                    <Link href="/privacy-policy" className={styles.link}>
+                        Privacy Policy
+                    </Link>
+                    <Link href="/terms-of-service" className={styles.link}>
+                        Terms of Service
+                    </Link>
+                    <Link href="/returns-plolicy" className={styles.link}>
+                        Returns Policy
+                    </Link>
+                </div>
             </nav>
             <Image
                 src="/footer-lockup.png"

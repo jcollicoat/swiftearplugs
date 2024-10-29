805887469c9399b991538aab504fa92bf8806fd9
Joseph Collicoat <jcollicoat@gmail.com>
2024-10-29 17:16:06 +0000

    add gtm tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './layout.scss';
 import { cookies } from 'next/headers';
 import { Footer } from '@Layout/Footer/Footer';
 import { Header } from '@Layout/Header/Header';
+import { GoogleTagManager } from '@Tracking/GoogleTagManager';
 import { CartProvider } from 'context/cart';
 import { ModalProvider } from 'context/modal';
 import { getCart } from 'shopify/cart';
@@ -40,6 +41,7 @@ export default function RootLayout({
                         <Footer />
                     </ModalProvider>
                 </CartProvider>
+                <GoogleTagManager />
             </body>
         </html>
     );
---
+++ components/Tracking/GoogleTagManager.tsx
@@ -0,0 +1,26 @@
+import Script from 'next/script';
+import { FC } from 'react';
+
+const { GTM_ID } = process.env;
+
+export const GoogleTagManager: FC = () => {
+    if (!GTM_ID) return null;
+
+    return (
+        <>
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+            />
+            <Script id="gtag-track" strategy="afterInteractive">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GTM_ID}');
+            `}
+            </Script>
+        </>
+    );
+};

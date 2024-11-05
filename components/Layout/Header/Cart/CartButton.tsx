7390839f16166a1479450ac4c8677dbbc2aa97f2
Joseph Collicoat <jcollicoat@gmail.com>
2024-11-05 13:38:01 +0000

    track cart views

--- components/Layout/Header/Cart/CartButton.tsx
+++ components/Layout/Header/Cart/CartButton.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from 'react';
 import { PiShoppingBag } from 'react-icons/pi';
+import { useFacebookPixel } from '@Tracking/FacebookPixel';
 import { content } from 'content';
 import { useCart } from 'context/cart';
 import { useModal } from 'context/modal';
@@ -9,7 +10,8 @@ import styles from './CartButton.module.scss';
 
 export const CartButton: FC = () => {
     const { cart } = useCart();
-    const { isOpen, setIsOpen } = useModal();
+    const { openModal } = useModal();
+    const { trackCustomFacebookEvent } = useFacebookPixel();
 
     // TODO: extract this logic
     const cartHasItems = (cart?.totalQuantity ?? 0) > 0;
@@ -19,13 +21,21 @@ export const CartButton: FC = () => {
     let sub = undefined;
     if (cartHasItems) {
         text = content.header.button;
-        sub = `${cart?.totalQuantity} items`;
+        sub = `${cart?.totalQuantity} ${cart?.totalQuantity === 1 ? 'item' : 'items'}`;
     }
 
     return (
         <button
             className={styles.button}
-            onClick={() => setIsOpen(!isOpen)}
+            onClick={() => {
+                openModal();
+                trackCustomFacebookEvent('ViewCart', {
+                    cart: cart?.lines.map((line) => ({
+                        id: line.merchandise.id,
+                        quantity: line.quantity,
+                    })),
+                });
+            }}
             disabled={buttonDisabled}
             aria-disabled={buttonDisabled}
         >
